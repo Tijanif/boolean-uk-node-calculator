9b3536f0e9c72e34d9aa81be7fc1499c58cf2087
Tijani Fulani <Fulani07@gmail.com>
2021-07-19 17:37:23 +0100

    simple calc

--- calculator.js
+++ calculator.js
@@ -6,12 +6,30 @@ const parseArgs = (arguments) => {
 
 const input = parseArgs(process.argv)
 
-const firstNumber = Number(input[0])
+
+
+
+const calculator = ({...args}) => {
+ const firstNumber = Number(input[0])
 const Operator = input[1]
 const secondNumber = Number(input[2])
 
-if(Operator === 'pluss')
-function pluss (number1, number2){
- return number1 + number2
+if(Operator === 'plus'){
+ return firstNumber + secondNumber
 }
-console.log(`Your result is: ${pluss(firstNumber, secondNumber)}`);
+
+if(Operator === 'minus'){
+ return firstNumber - secondNumber
+}
+
+if(Operator === 'times'){
+ return firstNumber * secondNumber
+}
+
+if(Operator === 'divide'){
+ return firstNumber / secondNumber
+}
+
+}
+
+console.log(`Your result is: ${calculator(input)}`);
